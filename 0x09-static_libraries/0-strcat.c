2e6c5089055cc4d0068828bd6414ffed3fc16c16
Philbert2022 <nshimyifilba@gmail.com>
2023-04-16 01:55:03 -0700

    updated on 0x09. C - Static libraries

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strcat - check the code
+* @dest: First argument
+* @src: Second argument
+* Return: a pointer of the resulting string
+*/
+
+char *strcat(char *dest, char *src)
+{
+int i, j;
+
+i = 0;
+while (dest[i] != '\0')
+i++;
+/*i -= 1;*/
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[i] = src[j];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
