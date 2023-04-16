b4605a33d65de4d9e96281973259faa8fb8a6e87
Philbert2022 <nshimyifilba@gmail.com>
2023-04-16 03:03:24 -0700

    updated 0x09-static_libraries

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -7,18 +7,21 @@
 * Return: a pointer of the resulting string
 */
 
-char *strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 int i, j;
 
 i = 0;
 while (dest[i] != '\0')
+{
 i++;
-/*i -= 1;*/
-for (j = 0; src[j] != '\0'; j++)
+}
+j = 0;
+while (src[j] != '\0')
 {
 dest[i] = src[j];
 i++;
+j++;
 }
 dest[i] = '\0';
 return (dest);
